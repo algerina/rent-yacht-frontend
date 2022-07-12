b9768d06e98148bf21849d0a72f41c190bfbbd65
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-07-12 23:32:32 +0100

    Add login and logout methods for auth.js reducer

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -40,3 +40,53 @@ export const signupUser = (credentials) => {
     });
   };
 };
+
+export const loginUser = (credentials) => {
+  return (dispatch) => {
+    return fetch("http://localhost:3001/login", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: credentials }),
+    }).then((res) => {
+      if (res.ok) {
+        setToken(res.headers.get("Authorization"));
+        return res
+          .json()
+          .then((userJson) =>
+            dispatch({ type: AUTHENTICATED, payload: userJson })
+          );
+      } else {
+        return res.json().then((errors) => {
+          dispatch({ type: NOT_AUTHENTICATED });
+          return Promise.reject(errors);
+        });
+      }
+    });
+  };
+};
+
+export const logoutUser = () => {
+  return (dispatch) => {
+    return fetch("http://localhost:3001/logout", {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: getToken(),
+      },
+    }).then((res) => {
+      if (res.ok) {
+        return dispatch({ type: NOT_AUTHENTICATED });
+      } else {
+        return res.json().then((errors) => {
+          dispatch({ type: NOT_AUTHENTICATED });
+          return Promise.reject(errors);
+        });
+      }
+    });
+  };
+};
+
