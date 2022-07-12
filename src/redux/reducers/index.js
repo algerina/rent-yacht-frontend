16b71c290867c0670723c0f32b1825d58b764896
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-07-12 22:49:28 +0100

    Setup store and reducers for authentication

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,15 @@
+// src/index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from 'react-redux'
+import store from '.reducers/store'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
