576d10df2b02dd705aa3a98f8f04fbc17abdccde
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-14 00:39:53 +0300

    modify singup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,55 +3,60 @@ import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import { signupUser } from '../redux/actions/auth';
+import './registeration-form.css';
+import logo from '../assets/Yacht-logo.svg';
 
 const Signup = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm();
   const onFormSubmit = (data) => {
-    console.log(data);
     dispatch(signupUser(data));
     navigate('/');
   };
 
   return (
-    <main className="">
-      <div className="">
-        <div className="">
+    <main className="main">
+      <div className="showcase">
+        <div className="logo"><img src={logo} alt="logo" /></div>
+        <div className="center">
           <div className="">
-            <h2 className="">Sign Up</h2>
+            <h1 className="">Sign Up</h1>
           </div>
-          <form className="" onSubmit={handleSubmit(onFormSubmit)}>
-            <div className="">
+          <form className="form" onSubmit={handleSubmit(onFormSubmit)}>
+            <div className="inputbox">
               <input
                 className=""
                 type="text"
                 placeholder="Username"
                 {...register('username', { required: 'Username is required' })}
               />
+              <span>Username</span>
             </div>
-            <div className="">
+            <div className="inputbox">
               <input
-                className=""
+                className="name"
                 type="email"
                 placeholder="Email"
                 {...register('email', { required: 'Email is required' })}
               />
+              <span>Email</span>
             </div>
-            <div className="">
+            <div className="inputbox">
               <input
-                className=""
+                className="score"
                 type="password"
                 placeholder="Password"
                 {...register('password', { required: 'Password is required' })}
               />
+              <span>Password</span>
             </div>
-            <input
-              className=""
-              type="submit"
-              value="Sign Up"
-            />
-            <Link className="" to="/login">Log In</Link>
+            <div className="submit">
+              <input type="submit" value="Submit" />
+              <Link className="login" to="/login">
+                Log In
+              </Link>
+            </div>
           </form>
         </div>
       </div>
