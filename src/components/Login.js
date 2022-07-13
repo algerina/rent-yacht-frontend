9dfc71f1097c5241ee798f2328c10d80d8e74762
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-14 00:39:40 +0300

    Modify the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,36 +6,63 @@ import { useForm } from 'react-hook-form';
 import { Link, Navigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../redux/actions/auth';
+import './registeration-form.css';
+import logo from '../assets/Yacht-logo.svg';
 
 const Login = ({ loggedIn }) => {
   if (loggedIn) return <Navigate to="/" replace />;
 
   const dispatch = useDispatch();
   const [error, setError] = useState(null);
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onFormSubmit = (data) => dispatch(loginUser(data)).catch(() => setError('Invalid credentials. Try again'));
 
   return (
-    <main className="">
-      <div className="">
-        <div className="">
-          <h2 className="">LOG IN</h2>
-        </div>
+    <main className="main">
+      <div className="showcase">
         {error && <p className="">{error}</p>}
         {errors.username && <p className="">Username is required</p>}
         {errors.email && <p className="">Email is required</p>}
         {errors.password && <p className="">Password is required</p>}
-        <form className="" onSubmit={handleSubmit(onFormSubmit)}>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="center">
           <div className="">
-            <input className="" type="username" placeholder="Username" {...register('username', { required: 'Username is required' })} />
+            <h1 className="">Sign Up</h1>
           </div>
-          <div className="">
-            <input className="" type="password" placeholder="Password" {...register('password', { required: 'Password is required' })} />
-          </div>
-          <input className="" type="submit" value="Log In" />
-          <Link className="" to="/signup">Sign Up</Link>
-        </form>
+          <form className="form" onSubmit={handleSubmit(onFormSubmit)}>
+            <div className="inputbox">
+              <input
+                className=""
+                type="text"
+                placeholder="Username"
+                {...register('username', { required: 'Username is required' })}
+              />
+              <span>Username</span>
+            </div>
+            <div className="inputbox">
+              <input
+                className="score"
+                type="password"
+                placeholder="Password"
+                {...register('password', { required: 'Password is required' })}
+              />
+              <span>Password</span>
+            </div>
+            <div className="submit">
+              <input type="submit" value="Submit" />
+              <Link className="login" to="/signup">
+                Sign up
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </main>
   );
