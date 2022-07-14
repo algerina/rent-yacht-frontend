389baeb3ac188255b24a6399dce301b9e68655b7
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-07-14 00:44:58 -0300

    add routes and bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ProtectedRoutes from './components/ProtectedRoutes';
 import useAuth from './hooks/useAuth';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import DeleteYachts from './components/admin-ui/DeleteYachts';
+import AddYachts from './components/admin-ui/AddYachts';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const { authChecked, loggedIn } = useAuth();
@@ -12,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login loggedIn={loggedIn} />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/add" element={<AddYachts />} />
+        <Route path="/delete" element={<DeleteYachts />} />
         <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
           <Route path="/" element={<h1>Yachts Page</h1>} />
         </Route>
