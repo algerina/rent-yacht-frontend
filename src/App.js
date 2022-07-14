affc90632cd7040f3096aefd6d7dd59fe8706d96
Estebanmual <esteban.munozal@gmail.com>
2022-07-14 13:51:23 -0500

    Add navbar for correct routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import ProtectedRoutes from './components/ProtectedRoutes';
 import useAuth from './hooks/useAuth';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import WithNav from './components/Navbar/WithNav';
+import WithoutNav from './components/Navbar/WithoutNav';
 
 function App() {
   const { authChecked, loggedIn } = useAuth();
@@ -10,10 +14,14 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login loggedIn={loggedIn} />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
-          <Route path="/" element={<h1>Yachts Page</h1>} />
+        <Route element={<WithoutNav />}>
+          <Route path="/login" element={<Login loggedIn={loggedIn} />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+        <Route element={<WithNav />}>
+          <Route element={<ProtectedRoutes isAllowed={loggedIn} authChecked={authChecked} redirectPath="/login" />}>
+            <Route path="/" element={<h1 />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
