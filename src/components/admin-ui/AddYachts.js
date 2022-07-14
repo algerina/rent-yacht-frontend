30a9f792f755931c24b4dfe137746ffb50ffba1c
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-07-14 00:45:18 -0300

    add yachts comonent

--- src/components/admin-ui/AddYachts.js
+++ src/components/admin-ui/AddYachts.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import logo from '../../assets/Yacht-logo.svg';
 
 function AddYachts() {
   const [yacht, setYacht] = useState({});
@@ -9,19 +10,26 @@ function AddYachts() {
   const handleChange = (e) => {
     e.preventDefault();
     setYacht({ ...yacht, [e.target.name]: e.target.value });
+    console.log(yacht);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    const img = document.getElementById('image');
+    const data = new FormData();
+    const img = document.getElementById('image_url');
 
-    formData.append('image', img.files[0]);
-    formData.append('name', yacht.name);
-    formData.append('description', yacht.description);
-    formData.append('price', yacht.price);
+    data.append('image_url', img.files[0]);
+    data.append('name', yacht.name);
+    data.append('description', yacht.description);
+    data.append('price', yacht.price);
+    console.log(data);
+    console.log(data.get('image_url'));
 
-    axios.post('http://127.0.0.1:3001/v1/yachts/', { formData })
+    axios.post('http://127.0.0.1:3001/v1/yachts/', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
       .then((response) => {
         console.log(response);
       })
@@ -31,27 +39,33 @@ function AddYachts() {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formAddYacht">
-        <Form.Label>Yacht Name</Form.Label>
-        <Form.Control type="text" htmlFor="name" name="name" placeholder="Enter Yacht Name" onChange={handleChange} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formAddYacht">
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="textarea" htmlFor="descripion" name="description" placeholder="Enter Yacht Description" onChange={handleChange} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formAddYacht">
-        <Form.Label>Price</Form.Label>
-        <Form.Control type="number" htmlFor="price" name="price" placeholder="Enter Yacht Price" onChange={handleChange} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formAddYacht">
-        <Form.Label>Image</Form.Label>
-        <Form.Control type="file" id="image" name="image" className="image" placeholder="Image" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        ADD YACHT
-      </Button>
-    </Form>
+    <main className="main">
+      <div className="effect" />
+      <div className="showcase">
+        <div className="logo"><img src={logo} alt="logo" /></div>
+        <Form onSubmit={handleSubmit} className="bg-light rounded-3 shadow p-4 bg-body">
+          <Form.Group className="mb-3" controlId="formAddYacht">
+            <Form.Label>Yacht Name</Form.Label>
+            <Form.Control type="text" htmlFor="name" name="name" placeholder="Enter Yacht Name" onChange={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formAddYacht">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="textarea" htmlFor="descripion" name="description" placeholder="Enter Yacht Description" onChange={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formAddYacht">
+            <Form.Label>Price</Form.Label>
+            <Form.Control type="number" htmlFor="price" name="price" placeholder="Enter Yacht Price" onChange={handleChange} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formAddYacht">
+            <Form.Label>Image</Form.Label>
+            <input type="file" id="image_url" name="image_url" placeholder="Select Image" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            ADD YACHT
+          </Button>
+        </Form>
+      </div>
+    </main>
   );
 }
 
