ccf63a1b4b45b1b153533787d92b757a7277216a
Omar Ragheb <ahmed.raph29@gmail.com>
2022-07-16 01:31:30 +0300

    fetch the yacht name in the table

--- src/components/reservations/SingleReservation.js
+++ src/components/reservations/SingleReservation.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 import { getToken } from '../../redux/actions/auth';
 
 function SingleReservation({
-  city, cost, id, startDate, daysNumber,
+  city, cost, id, startDate, daysNumber, yachtId,
 }) {
   const navigate = useNavigate();
+  const [yachtName, setYachtName] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`http://localhost:3001/v1/yachts/${yachtId}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: getToken(),
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setYachtName(data.name);
+      }
+    })();
+  }, []);
 
   const deleteReservation = async () => {
     const response = await fetch(`http://localhost:3001/v1/reservations/${id}`, {
@@ -19,18 +37,21 @@ function SingleReservation({
     });
     navigate(0);
     if (response.ok) {
-      console.log('Reservation deleted');
-    } else {
-      console.log(response);
+      navigate('/reservations');
     }
   };
 
   return (
     <tr>
-      <td className="px-3">{id + 1}</td>
-      <td>Athena</td>
+      <td>{yachtName}</td>
       <td>{city}</td>
-      <td>{startDate}</td>
+      <td>
+        {new Intl.DateTimeFormat('en-US', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+        }).format(new Date(startDate))}
+      </td>
       <td>{daysNumber}</td>
       <td>{cost}</td>
       <td>
@@ -48,6 +69,7 @@ SingleReservation.propTypes = {
   startDate: PropTypes.string.isRequired,
   daysNumber: PropTypes.number.isRequired,
   cost: PropTypes.number.isRequired,
+  yachtId: PropTypes.number.isRequired,
 };
 
 export default SingleReservation;
