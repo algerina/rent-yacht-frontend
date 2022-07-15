2e7dc67ea8720573d21d9381a52195fe20ae63c5
German Renato Cobian <germancobian@hotmail.com>
2022-07-15 12:12:32 -0700

    Add styling to reservations component

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import getReservations from '../redux/actions/reservations';
 import SingleReservation from './SingleReservation';
+import './reservations-display.css';
 
 const Reservations = () => {
   const dispatch = useDispatch();
@@ -13,30 +14,36 @@ const Reservations = () => {
   const reservations = useSelector((state) => state.reservations);
 
   return (
-    <table className="table table-hover">
-      <thead className="text-white bg-dark text-center">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">City</th>
-          <th scope="col">Start date</th>
-          <th scope="col">Number of days</th>
-          <th scope="col">Cost</th>
-          <th scope="col">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {reservations.map((reservation, id) => (
-          <SingleReservation
-            key={id}
-            id={reservation.id}
-            city={reservation.city}
-            startDate={reservation.start_date}
-            daysNumber={reservation.days_number}
-            cost={reservation.cost}
-          />
-        ))}
-      </tbody>
-    </table>
+    <main className="main">
+      <div className="background">
+        <div className="foreground">
+          <table className="table table-hover">
+            <thead className="text-white bg-primary">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">City</th>
+                <th scope="col">Start date</th>
+                <th scope="col">Number of days</th>
+                <th scope="col">Cost</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {reservations.map((reservation, id) => (
+                <SingleReservation
+                  key={id}
+                  id={reservation.id}
+                  city={reservation.city}
+                  startDate={reservation.start_date}
+                  daysNumber={reservation.days_number}
+                  cost={reservation.cost}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
   );
 };
 
