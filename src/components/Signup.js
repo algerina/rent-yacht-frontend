32a94147209425ccca529694f4d265f4f37985a3
Estebanmual <esteban.munozal@gmail.com>
2022-07-18 18:10:32 -0500

    Fixed validation toasts for the registration and signing up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import { signupUser } from '../redux/actions/auth';
 import './registeration-form.css';
 import logo from '../assets/Yacht-logo.svg';
@@ -9,14 +11,52 @@ import logo from '../assets/Yacht-logo.svg';
 const Signup = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [signUpError, setSignUpError] = useState([]);
+
+  const notifyError = (error) => toast.error(error, {
+    position: 'top-right',
+    autoClose: 15000,
+    pauseOnHover: true,
+    draggable: true,
+  });
+
   const onFormSubmit = (data) => {
-    dispatch(signupUser(data));
-    navigate('/');
+    if (data.password.length >= 6) {
+      dispatch(signupUser(data)).then(navigate('/')).catch((err) => notifyError(err.error));
+    } else {
+      notifyError('Password must be at least 6 characters long');
+    }
   };
 
+  const isEmpty = Object.keys(errors).length === 0;
+
+  const updateErrors = () => {
+    const newErrors = [];
+    handleSubmit(onFormSubmit);
+    if (!isEmpty) {
+      Object.entries(errors).forEach(([key, value]) => {
+        newErrors.push([key, value.message]);
+      });
+      setSignUpError(newErrors);
+    }
+  };
+
+  useEffect(() => {
+    if (signUpError.length !== 0) {
+      console.log(signUpError);
+      signUpError.forEach((error) => notifyError(error[1]));
+    }
+  }, [signUpError]);
+
   return (
-    <main className="main">
+    <div className="main">
       <div className="effect" />
       <div className="showcase">
         <div className="logo"><img src={logo} alt="logo" /></div>
@@ -53,7 +93,7 @@ const Signup = () => {
               <span>Password</span>
             </div>
             <div className="submit">
-              <input type="submit" value="Submit" />
+              <input type="submit" value="Submit" onClick={() => updateErrors()} />
               <Link className="login" to="/login">
                 Log In
               </Link>
@@ -61,7 +101,8 @@ const Signup = () => {
           </form>
         </div>
       </div>
-    </main>
+      <ToastContainer />
+    </div>
   );
 };
 
