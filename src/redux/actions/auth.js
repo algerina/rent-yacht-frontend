04358ada67ef3fdf433b4c10e0c97f329fd59554
Estebanmual <esteban.munozal@gmail.com>
2022-07-20 03:49:42 -0500

    Solve store bug

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -58,6 +58,7 @@ export const loginUser = (credentials) => (dispatch) => fetch('http://localhost:
   body: JSON.stringify({ user: credentials }),
 }).then((res) => {
   if (res.ok) {
+    console.log(res);
     setToken(res.headers.get('Authorization'));
     return res
       .json()
--- src/redux/storeSetUp.js
+++ src/redux/storeSetUp.js
@@ -4,10 +4,15 @@ import {
 import thunk from 'redux-thunk';
 import authReducer from './reducers/auth';
 import yachtReducer from './reducers/yachtReducer';
+import reservationsReducer from './reducers/reservations';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = combineReducers({ auth: authReducer, yacht: yachtReducer });
+const rootReducer = combineReducers({
+  auth: authReducer,
+  yacht: yachtReducer,
+  reservations: reservationsReducer,
+});
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
