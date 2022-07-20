8bab5193393cf08fba929c093076e444c0d8a519
Estebanmual <esteban.munozal@gmail.com>
2022-07-20 04:00:20 -0500

    Resolve lilnters errors

--- src/components/yachts/Yachtshow.js
+++ src/components/yachts/Yachtshow.js
@@ -28,29 +28,37 @@ const Yachtshow = () => {
       });
   }, []);
 
-
   if (yacht) {
     return (
-<div className="yacht-card">
-  <div className="img-wrap">
-  <img className="single-yacht" src={yacht.image_url} alt="single-yacht" />
-  </div>
-  <div className="card-button"> 
-<Card className="text-box" style={{ width: '15rem' }}>
-      <Card.Header><span className="name">{yacht.name}</span></Card.Header>
-      <ListGroup variant="flush">
-        <ListGroup.Item><span>Description: </span>{yacht.description}</ListGroup.Item>
-        <ListGroup.Item> <span>Price: </span> ${yacht.price}</ListGroup.Item>
-      </ListGroup>
-    </Card>
-    <Button  className="show-button" type="button" style={{ width: '15rem' }}>
-              <Link to={`/reserve/${yacht.id}`} className="reserve" id={yacht.id}>
+      <div className="yacht-card">
+        <div className="img-wrap">
+          <img className="single-yacht" src={yacht.image_url} alt="single-yacht" />
+        </div>
+        <div className="card-button">
+          <Card className="text-box" style={{ width: '15rem' }}>
+            <Card.Header><span className="name">{yacht.name}</span></Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <span>Description: </span>
+                {yacht.description}
+              </ListGroup.Item>
+              <ListGroup.Item>
                 {' '}
-                Reserve
-              </Link>
-              </Button>
-              </div>
-</div>
+                <span>Price: </span>
+                {' '}
+                $
+                {yacht.price}
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+          <Button className="show-button" type="button" style={{ width: '15rem' }}>
+            <Link to={`/reserve/${yacht.id}`} className="reserve" id={yacht.id}>
+              {' '}
+              Reserve
+            </Link>
+          </Button>
+        </div>
+      </div>
     );
   }
   return (<h1>Page not found!</h1>);
